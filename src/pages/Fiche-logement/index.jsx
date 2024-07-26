02cf3878435e0f2f413ecbac47934ccf383a9f12
RudiDonnot <149707597+RudiDonnot@users.noreply.github.com>
2024-07-26 18:33:55 +0200

    bug 404 fiche logement finalisé

--- src/pages/Fiche-logement/index.jsx
+++ src/pages/Fiche-logement/index.jsx
@@ -5,6 +5,7 @@ import "./Fiche-logement.css";
 import { useParams } from "react-router-dom";
 import Rating from "../../components/Rating/index.jsx";
 import Carrousel from "../../components/Carrousel/index.jsx";
+import Error from "../../components/Error/index.jsx";
 
 function Fichelogement() {
   const { id } = useParams();
@@ -15,57 +16,64 @@ function Fichelogement() {
     setLogement(filteredLogements);
   }, [id]);
 
-  return (
-    <div className="Logementpart">
-      <div>
-        <Carrousel pictures={logement?.pictures} />
-        <div className="titre">
-          <div className="leftpart">
-            <h2>{logement?.title}</h2>
-            <p>{logement?.location}</p>
+  if (logement) {
+    return (
+      <div className="Logementpart">
+        <div>
+          <Carrousel pictures={logement?.pictures} />
+          <div className="titre">
+            <div className="leftpart">
+              <h2>{logement?.title}</h2>
+              <p>{logement?.location}</p>
+            </div>
+            <div className="rightpart">
+              <p className="hostname">{logement?.host.name}</p>
+              <img
+                className="hostpicture"
+                src={logement?.host.picture}
+                alt=""
+              ></img>
+            </div>
           </div>
-          <div className="rightpart">
-            <p className="hostname">{logement?.host.name}</p>
-            <img
-              className="hostpicture"
-              src={logement?.host.picture}
-              alt=""
-            ></img>
-          </div>
-        </div>
-        <div className="tagsrating">
-          <div className="tags">
-            {logement?.tags.map((tags, index) => {
-              return (
-                <div key={index}>
-                  <p className="tag">{tags}</p>
-                </div>
-              );
-            })}
-          </div>
-          <Rating rating={logement?.rating} />
-        </div>
-        <div className="descriptionequipement">
-          <div className="description">
-            <Retractcards title="Description" content={logement?.description} />
-          </div>
-          <div className="equipement">
-            <Retractcards
-              title="Equipement"
-              content={logement?.equipments.map((item, index) => {
+          <div className="tagsrating">
+            <div className="tags">
+              {logement?.tags.map((tags, index) => {
                 return (
                   <div key={index}>
-                    <p>{item}</p>
+                    <p className="tag">{tags}</p>
                   </div>
                 );
               })}
-              className="vignette"
-            />
+            </div>
+            <Rating rating={logement?.rating} />
+          </div>
+          <div className="descriptionequipement">
+            <div className="description">
+              <Retractcards
+                title="Description"
+                content={logement?.description}
+              />
+            </div>
+            <div className="equipement">
+              <Retractcards
+                title="Equipement"
+                content={logement?.equipments.map((item, index) => {
+                  return (
+                    <div key={index}>
+                      <p>{item}</p>
+                    </div>
+                  );
+                })}
+                className="vignette"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <Error />;
+  }
 }
 
 export default Fichelogement;
